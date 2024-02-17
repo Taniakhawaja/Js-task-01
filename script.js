2056235b27be0016c9aa3c178a7166405d0ec1ef
Tania-khawaja <145832884+Taniakhawaja@users.noreply.github.com>
2024-02-17 12:41:54 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// exercise1//
+
+/*const userName = prompt("Enter your name");
+function welcome(){
+    alert("Welcome" +userName);
+}
+welcome ();*/
+
+//exercise2//
+/*let quote =["keep going"];
+function generateQuote(){
+    return quote[0];
+}
+let randomQuote = generateQuote();
+console.log(randomQuote)*/
+
+//exercise3//
+ function generateRandomNumber(){
+    return Math.floor(Math.random() * 100) + 1;
+ }
+ const randomNumber = generateRandomNumber();
+ console.log(randomNumber);
