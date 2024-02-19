14de974e35c77d7d1e9b36bd4fef2d4e9b494d8b
Tania-khawaja <145832884+Taniakhawaja@users.noreply.github.com>
2024-02-19 06:52:39 -0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,48 +1,29 @@
-// exercise 1 //
+// exercise1//
 
- /*let myNum = 493;
-let sum = 0;
-while(myNum){
-    sum += myNum % 10;
-    myNum = (myNum / 10) | 0;
+/*const userName = prompt("Enter your name");
+function welcome(){
+    alert("Welcome" +userName);
 }
-    console.log(sum);*/
+welcome ();*/
 
-    // exercise 2 //
-/*const Num1 = [1,2,3];
-const Num2 = [4,5,6];
-let sum = 0;
-for(let i = 0 ; i < Num1.length ; i++ ){
-    sum += Num1[i] + Num2[i];
-}
-console.log(sum)*/
+//exercise2//
+/*const quotes = [
+    "Quote 1",
+    "Quote 2",
+    "Quote 3"
+  ];
+  
+  function generateQuote() {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const quote = quotes[randomIndex];
+    console.log(quote);
+  }
+  
+  generateQuote();*/
 
-// exercise 3 //
-
-/*first calculate the sum of the numbers in the arrays using a for loop then 
-devides it by the num of elements to find average*/
-
-/*const numbers = [2,3,4,5];
-let sum = 0;
-for(let i = 0;  i < numbers.length; i ++) {
-        sum += numbers[i];
-    }
-    const average = sum / numbers.length;
-    console.log(average);*/
-
-     // exercise 4 //
-/*it counts the characters of strings and  print them out through console */
-
-     /*const string1 = "car";
-     const string2 = "care";
-     if
-       (string1.length === string2.length) {
-        console.log("strings are equal");
-     }
-     else {
-        console.log("strings are not equal");
-    }*/
-
-    // exercise 5 //
-
-    
+//exercise3//
+ function generateRandomNumber(){
+    return Math.floor(Math.random() * 100) + 1;
+ }
+ const randomNumber = generateRandomNumber();
+ console.log(randomNumber);
